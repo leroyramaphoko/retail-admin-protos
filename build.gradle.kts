10f86372f09a77e01f798ded07995426c1522b9f
Leroy Ramaphoko <adventapp777@gmail.com>
2026-01-17 13:15:57 +0200

    Dummy proto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    `maven-publish`
+    id("com.google.protobuf") version "0.9.4"
 }
 
 group = "com.apptorise.retail.core.api"
@@ -10,6 +12,8 @@ repositories {
 }
 
 dependencies {
+    implementation("com.google.protobuf:protobuf-java:3.25.1")
+    implementation("com.google.protobuf:protobuf-kotlin:3.25.1")
     testImplementation(kotlin("test"))
 }
 
@@ -17,6 +21,46 @@ kotlin {
     jvmToolchain(21)
 }
 
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.25.1"
+    }
+    generateProtoTasks {
+        all().forEach { task ->
+            task.builtins {
+                // Java is enabled by default, no need to call 'id' or 'create'
+                create("kotlin")
+            }
+        }
+    }
+}
+
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            artifactId = "retail-admin-protos"
+
+            pom {
+                name.set("Retail Admin Protos")
+                description.set("Protocol Buffer library for retail administration")
+                url.set("https://github.com/apptorise/retail-admin-protos")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+            }
+        }
+    }
 }
